8f5e9880e8d66648fdce000388405992f6fce719
Manuel Vázquez Acosta <manu@merchise.org>
2017-01-12 09:46:04 -0500

    It's 2017!

--- xopgi/xopgi_backports/__openerp__.py
+++ xopgi/xopgi_backports/__openerp__.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------
 # xopgi.backports
 # ---------------------------------------------------------------------
-# Copyright (c) 2014-2016 Merchise Autrement [~º/~]
+# Copyright (c) 2014-2017 Merchise Autrement [~º/~]
 # All rights reserved.
 #
 # This is free software; you can redistribute it and/or modify it under the
