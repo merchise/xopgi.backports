5ed7def39494af4683c9f7fb728f16a4adb9c640
Manuel Vázquez Acosta <manu@merchise.org>
2017-06-21 12:16:45 -0400

    Mark ready for Odoo 9.

--- xopgi/xopgi_backports/__openerp__.py
+++ xopgi/xopgi_backports/__openerp__.py
@@ -21,7 +21,7 @@
 
     # Warning:  This addon should only contain res_config items.
 
-    "version": "2015.06.01",
+    "version": "2017.06.21",
 
     "author": "Merchise Autrement",
     "website": "http://www.merchise.org/addons/xopgi_backports",
@@ -35,7 +35,7 @@
 
     # MIGRATION POLICY: All addons are not included until someone work on them
     # and upgrade them.
-    'installable': (8, 0) <= ODOO_VERSION_INFO < (9, 0),   # noqa
+    'installable': (8, 0) <= ODOO_VERSION_INFO < (10, 0),   # noqa
 
     'auto_install': True,
 }
--- xopgi/xopgi_backports/config.py
+++ xopgi/xopgi_backports/config.py
@@ -16,16 +16,15 @@ from __future__ import (division as _py3_division,
                         absolute_import as _py3_abs_import)
 
 
-from openerp.osv import fields, orm
+from xoeuf import fields, models
 
 
-class XopgiBackportConfig(orm.TransientModel):
+class XopgiBackportConfig(models.TransientModel):
     _name = 'xopgi.backports.config.settings'
     _inherit = 'res.config.settings'
 
-    _columns = {
-        'module_xopgi_partner_merge':
-            fields.boolean("Allow to merger partners",
-                           help="Install the module xopgi_partner_merge, "
-                           "which is a backport from Odoo's similar feature.")
-    }
+    module_xopgi_partner_merge = fields.Boolean(
+        "Allow to merger partners",
+        help=("Install the module xopgi_partner_merge, "
+              "which is a backport from Odoo's similar feature.")
+    )
