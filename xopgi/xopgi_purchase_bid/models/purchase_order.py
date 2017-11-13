7db1ee93c4e01c03d86ed8f73cd8ac98240fe0da
Manuel Vázquez Acosta <manuel@merchise.org>
2017-11-13 11:46:19 -0500

    Fix flakes.

--- xopgi/xopgi_purchase_bid/models/purchase_order.py
+++ xopgi/xopgi_purchase_bid/models/purchase_order.py
@@ -68,10 +68,7 @@ class PurchaseOrder(models.Model):
                 )
             order._add_supplier_to_product()
             # Deal with double validation process
-            if order.company_id.po_double_validation == 'one_step'\
-                   or (order.company_id.po_double_validation == 'two_step'\
-                   and order.amount_total < self.env.user.company_id.currency_id.compute(order.company_id.po_double_validation_amount, order.currency_id))\
-                   or order.user_has_groups('purchase.group_purchase_manager'):
+            if order.company_id.po_double_validation == 'one_step' or (order.company_id.po_double_validation == 'two_step' and order.amount_total < self.env.user.company_id.currency_id.compute(order.company_id.po_double_validation_amount, order.currency_id)) or order.user_has_groups('purchase.group_purchase_manager'):  # noqa
                 order.button_approve()
             else:
                 order.write({'state': 'to approve'})
