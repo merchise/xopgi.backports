dad9439869d837904047a4150aa7e27963ff278c
Monica Diaz Pena <monicadp@merchise.org>
2018-02-26 10:52:08 -0500

    Remove Odoo 8 and 9 support.

--- xopgi/xopgi_project_state/__manifest__.py
+++ xopgi/xopgi_project_state/__manifest__.py
@@ -1,1 +1,25 @@
-__openerp__.py
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can do what the LICENCE file allows you to.
+#
+
+dict(
+    name='xopgi_project_state',
+    summary='Resurrects the state in projects',
+    depends=['project'],
+    data=[
+        'views/project.xml',
+    ],
+
+    # This is installable from Odoo 8+ just for us to be able to have it
+    # installed before migrating the DB.  Otherwise, OpenUpgrade won't install
+    # it and we may loose the data while migrating.
+    installable=10 <= MAJOR_ODOO_VERSION < 11,  # noqa
+
+    # Only install this, if someone requires it.
+    auto_install=False,
+)
